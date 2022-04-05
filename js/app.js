fd62ef93c329b72a43077dfd0a0b06ac63165092
golafoned <golafoned@gmail.com>
2022-04-05 14:29:03 +0300

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -1,85 +1,66 @@
-let div = document.querySelector('#pic')
-let img_js = document.createElement("img")
-img_js.setAttribute('src', 'images/bg2.png')
-img_js.setAttribute('style', `width: 100%;
-margin-top: 0%;
-overflow: hidden;
-height: 53vw;
-box-sizing: border-box;
-animation: appear_img 2s;
-@keyframes appear_img {
-	from {opacity: 0;}
-	to {opacity: 1;}	
-}`)
-div.prepend(img_js)
-
-setInterval(() => {
-    // console.log(document.querySelector("#bg12").getAttribute("src") == "images/bg1.png")
-    if (img_js.getAttribute("src") == "images/bg1.png") {
-        // document.querySelector("#bg12").setAttribute('src', 'images/bg2.png')
-        img_js.setAttribute('src', 'images/bg2.png')
-        img_js.setAttribute('style', `
-width: 100%;
-margin-top: 0%;
-overflow: hidden;
-height: 53vw;
-box-sizing: border-box;
-animation: appear_img 2s;
-@keyframes appear_img {
-	from {opacity: 0;}
-	to {opacity: 1;}	
-}`)
-        div.prepend(img_js)
+function set_image() {
+    let img_style = `
+    width: 100%;
+    margin-top: 0%;
+    overflow: hidden;
+    height: 53vw;
+    box-sizing: border-box;
+    animation: appear_img 2s;
+    @keyframes appear_img {
+        from {opacity: 0;}
+        to {opacity: 1;}	
+    }`
+    let div = document.querySelector('#pic')
+    let img_js = document.createElement("img")
+    img_js.setAttribute('src', 'images/bg1.png')
+    img_js.setAttribute('style', img_style)
+    div.prepend(img_js)
+    setInterval(() => {
+        console.log(img_js.getAttribute("src"))
         img_js.remove()
+        if (img_js.getAttribute("src") == "images/bg1.png") {
+            
+            img_js.setAttribute('src', 'images/bg2.png')
+            img_js.setAttribute('style', img_style)
+            console.log(img_js)
+            div.prepend(img_js)
+            return
+    
+        }
+        if (img_js.getAttribute("src") == "images/bg2.png") {
+            img_js.setAttribute('src', 'images/bg3.png')
+            img_js.setAttribute('style', img_style)
+            div.prepend(img_js)
+            return
+        }
+        if (img_js.getAttribute("src") == "images/bg3.png") {
+            img_js.setAttribute('src', 'images/bg1.png')
+            img_js.setAttribute('style', img_style)
+            div.prepend(img_js)
+            return
+        }
+    }, 3000);
 
-    }
-    if (img_js.getAttribute("src") == "images/bg2.png") {
-        // document.querySelector("#bg12").setAttribute('src', 'images/bg3.png')
-        img_js.setAttribute('src', 'images/bg3.png')
-        img_js.setAttribute('style', `width: 100%;
-margin-top: 0%;
-overflow: hidden;
-height: 53vw;
-box-sizing: border-box;
-animation: appear_img 2s;
-@keyframes appear_img {
-	from {opacity: 0;}
-	to {opacity: 1;}	
-}`)
-        div.prepend(img_js)
-        img_js.remove()
-    }
-    if (img_js.getAttribute("src") == "images/bg3.png") {
-        // document.querySelector("#bg12").setAttribute('src', 'images/bg1.png')
-        img_js.setAttribute('src', 'images/bg1.png')
-        img_js.setAttribute('style', `width: 100%;
-margin-top: 0%;
-overflow: hidden;
-height: 53vw;
-box-sizing: border-box;
-animation: appear_img 2s;
-@keyframes appear_img {
-	from {opacity: 0;}
-	to {opacity: 1;}	
-}`)
-        div.prepend(img_js)
-        img_js.remove()
-    }
-}, 3000);
-setInterval(()=>{
+}
 
-}, 1)
+set_image()
 let time = document.querySelector('#glock')
 
-// console.log(hours , min, sex)
 setInterval(()=>{
     let date = new Date()
-let hours = date.getHours()
-let min = date.getMinutes()
-let sex = date.getSeconds()
-
-    time.innerHTML = `${hours} ${min} ${sex}`
-    console.log(sex)
+    let hours = date.getHours()
+    let min = date.getMinutes()
+    let sex = date.getSeconds()
+    if (Number(hours) < 10) {
+        hours = '0'+hours
+    } 
+    if (Number(min) < 10) {
+        min = '0'+min
+    } 
+    if (Number(sex) < 10) {
+        sex = '0'+sex
+    } 
+    time.innerHTML = `${hours}:${min}:${sex}`
 }, 1000)
 
 
